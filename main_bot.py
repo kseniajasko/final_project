4f5cca6135989693062bb39360c1dc87753972f4
kseniajasko@gmail.com <kseniajasko555>
2022-01-03 21:19:14 +0200

    prefinal commit

--- main_bot.py
+++ main_bot.py
@@ -1,59 +1,58 @@
-import json
-import logging
-import os
-from datetime import datetime, timedelta
+import logging, os
 
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update, ReplyKeyboardRemove
-from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, ConversationHandler, CallbackContext, \
-    MessageHandler, Filters
+from datetime import timedelta
+
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardRemove
+from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, ConversationHandler, MessageHandler, Filters
 from telegram_bot_pagination import InlineKeyboardPaginator
 
 import telegramcalendar
 from html_parsing import *
 
 
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+logging.basicConfig(filename='data/bot_info.log', format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
 logger = logging.getLogger(__name__)
 
 token = os.environ.get('TELEGRAM_TOKEN')
-updater = Updater(token)
+updater = Updater(token, use_context=True)
 
 dispatcher = updater.dispatcher
 
-FIRST, SECOND, THIRD = range(3)
+FIRST, SECOND = range(2)
 
 
 search_cities = dict()
 search_dates = dict()
+parsing_dict = dict()
 
 def start(update, context):
     user = update.message.from_user
     chat_id = user['id']
     logger.info(f'User  started {user.first_name} {user.last_name}, chat_id is {chat_id}')
     keyboard = [
-        [InlineKeyboardButton('Івано-Франківськ', callback_data='Ivano-Frankovsk'),
+        [InlineKeyboardButton('Івано-Франківськ', callback_data='ivano-frankovsk'),
          InlineKeyboardButton('Дніпро', callback_data='dnepropetr')],
 
-        [InlineKeyboardButton('Київ', callback_data='Kiev'),
-        InlineKeyboardButton('Львів', callback_data='Lviv')],
+        [InlineKeyboardButton('Київ', callback_data='kiev'),
+        InlineKeyboardButton('Львів', callback_data='lviv')],
 
         [InlineKeyboardButton('Луцьк', callback_data='lutsk'),
-        InlineKeyboardButton('Одеса', callback_data='Odessa')],
+        InlineKeyboardButton('Одеса', callback_data='odessa')],
 
-        # InlineKeyboardButton('Харків', callback_data='Kharkiv'),
-        # InlineKeyboardButton('Луцьк', callback_data='lutsk'),
+        [InlineKeyboardButton('Полтава', callback_data='poltava'),
+         InlineKeyboardButton('Ужгород', callback_data='ujgorod')],
 
-        [InlineKeyboardButton('Харків', callback_data='Kharkiv'),
+        [InlineKeyboardButton('Харків', callback_data='kharkiv'),
         InlineKeyboardButton('Чернівці', callback_data='chernovtsyi')
         ],
     ]
     reply_markup = InlineKeyboardMarkup(keyboard)
     update.message.reply_text(
-        text="Будь ласка, виберіть місто зі списку:",
+        text="Будь ласка, виберіть місто \U00002B07",
         reply_markup=reply_markup
     )
-    #update.bot.send_message(chat_id=update.callback_query.from_user.id, text="Please, select a city", reply_markup=reply_markup)
+
     return FIRST
 
 
@@ -62,10 +61,10 @@ def on_calendar(update, context):
     query.answer()
     search_cities[update.effective_chat.id] = query.data
     reply_markup = telegramcalendar.create_calendar()
-    #query.message.send_message(text='Please, select a date', reply_markup=reply_markup)
+
     query.bot.send_message(
         chat_id=update.effective_chat.id,
-        text='Будь ласка, виберіть дату:',
+        text='Будь ласка, виберіть дату \U00002B07',
         reply_markup=reply_markup
     )
 
@@ -81,13 +80,13 @@ def on_callback_query(update, context):
     selected, date = telegramcalendar.process_calendar_selection(bot, update)
 
     if selected:
-        if date < datetime.now() - timedelta(days=1):
+        if date < datetime.now() - timedelta(days=1) or date - datetime.now() > timedelta(days=59):
             logger.info(
-                f'User {update.effective_chat.id} selected wrong date')
+                f'User {update.effective_chat.id} selected wrong date ')
 
             query.bot.send_message(
                 chat_id=update.effective_chat.id,
-                text=f'Ви вибрали невірну дату. \nЩоб продовжити натисніть на /start.'
+                text=f'Ви вибрали невірну дату \U0000274C. \nЩоб продовжити натисніть на /start.'
             )
             return ConversationHandler.END
 
@@ -99,46 +98,22 @@ def on_callback_query(update, context):
 
             query.bot.send_message(
                 chat_id=update.effective_chat.id,
-                text=f'Натисніть на /result, щоб побачити результат.',
+                text=f'Ви вибрали дату: {search_dates[update.effective_chat.id]} \U00002705. \nНатисніть на /result, щоб побачити результат та почекайте \U0000270F.',
                 reply_markup=ReplyKeyboardRemove()
             )
 
             tmp_result = parsing_result(search_cities[update.effective_chat.id], search_dates[update.effective_chat.id])
-
-            tmp_result_str = '\n'.join(str(', '.join(e)) for e in tmp_result)
-
             tmp_list = new_text_view(tmp_result)
-
-            global pagination_list
-            pagination_list = [str('\n'.join(e)) for e in tmp_list]
-
-            # bot.send_message(
-            #     chat_id=update.effective_chat.id,
-            #     text='Введіть команду /result, щоб побачити результат.',
-            # )
-
-            # if len(tmp_result_str) > 4096:
-            #     for x in range(0, len(tmp_result_str), 4096):
-            #         bot.send_message(
-            #             chat_id=update.effective_chat.id,
-            #             text=tmp_result_str[x:x + 4096]
-            #         )
-            #
-            # else:
-            #     context.bot.send_message(
-            #         chat_id=update.effective_chat.id,
-            #         text=tmp_result_str
-            #
-            #     )
+            tmp_pagination_list = [str('\n'.join(e)) for e in tmp_list]
+            parsing_dict[update.effective_chat.id] = tmp_pagination_list
 
             return ConversationHandler.END
 
-
 def result_first_page(update, context):
-
     query = update.callback_query
+    logger.info(f'User {update.effective_chat.id} have got result')
 
-    # query.answer()
+    pagination_list = parsing_dict[update.effective_chat.id]
 
     paginator = InlineKeyboardPaginator(
         len(pagination_list),
@@ -153,13 +128,15 @@ def result_first_page(update, context):
 
     context.bot.send_message(
         chat_id=update.effective_chat.id,
-        text=f'Щоб вибрати місто та дату заново натисніть на /start \U0001F60A.'
+        text=f'Переключайте сторінки для того, щоб побачити весь розклад \U000023EA \U000025C0 \t \U000020E3 \t \U000025B6 \U000023E9   \nЩоб вибрати місто та дату заново натисніть на /start \U0001F60A.'
     )
 
 def result_page_callback(update, context):
     query = update.callback_query
 
     query.answer()
+
+    pagination_list = parsing_dict[update.effective_chat.id]
 
     page = int(query.data.split('#')[1])
 
@@ -175,11 +152,10 @@ def result_page_callback(update, context):
         parse_mode='Markdown'
     )
 
-
 def unknown(update, context):
     context.bot.send_message(
         chat_id=update.effective_chat.id,
-        text="Вибачте, я не знаю цієї команди."
+        text='Вибачте, я не знаю цієї команди.'
     )
 
 if __name__ == '__main__':
